43ae8532aed923558eb73596009d5606a6a5de33
Rylee George <ryleegeorge1@gmail.com>
2023-07-01 05:02:56 +1000

    Add more libs

--- benches/tag_reader.rs
+++ benches/tag_reader.rs
@@ -2,6 +2,11 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use lofty::{flac::FlacFile, Accessor, AudioFile, Probe, TaggedFileExt};
 use metaflac::{block::VorbisComment, Tag};
 use std::path::Path;
+use symphonia::core::formats::FormatOptions;
+use symphonia::core::io::MediaSourceStream;
+use symphonia::core::meta::{MetadataOptions, MetadataRevision, StandardTagKey};
+use symphonia::core::probe::Hint;
+
 pub struct AudioMetadata {
     pub name: String,
     pub album: String,
@@ -11,6 +16,55 @@ pub struct AudioMetadata {
     pub path: String,
     pub lossless: bool,
     pub duration: u64,
+}
+impl Default for AudioMetadata {
+    fn default() -> AudioMetadata {
+        AudioMetadata {
+            name: String::from(""),
+            album: String::from(""),
+            album_artist: String::from(""),
+            year: 0,
+            track: 0,
+            path: String::from(""),
+            lossless: false,
+            duration: 0,
+        }
+    }
+}
+pub fn get_metadata_metaflac_reader(path: String) -> AudioMetadata {
+    let file = std::fs::File::open(&path).unwrap();
+    let mut reader = std::io::BufReader::new(file);
+    let tag = Tag::read_from(&mut reader).unwrap();
+    let vorbis: &VorbisComment = tag.vorbis_comments().unwrap();
+
+    let mut stream_info = tag.get_blocks(metaflac::BlockType::StreamInfo);
+    let duration = match stream_info.next() {
+        Some(metaflac::Block::StreamInfo(s)) => Some(s.total_samples as u64 / s.sample_rate as u64),
+        _ => None,
+    };
+    let metadata: AudioMetadata = AudioMetadata {
+        name: vorbis
+            .title()
+            .map(|v| v[0].clone())
+            .unwrap_or_else(|| "FAILED TO READ TITLE DEAFTONE".to_string()),
+        album: vorbis
+            .album()
+            .map(|v| v[0].clone())
+            .unwrap_or_else(|| "FAILED TO READ ALBUM DEAFTONE".to_string()),
+        album_artist: match vorbis.album_artist().map(|v| v[0].clone()) {
+            Some(e) => e,
+            None => vorbis
+                .artist()
+                .map(|v| v[0].clone())
+                .unwrap_or_else(|| "FAILED TO READ ARTIST DEAFTONE".to_string()),
+        },
+        year: get_year(vorbis),
+        track: vorbis.track().unwrap_or(0),
+        path,
+        lossless: true,
+        duration: duration.unwrap_or_default(),
+    };
+    metadata
 }
 
 pub fn get_metadata_metaflac(path: String) -> AudioMetadata {
@@ -45,6 +99,152 @@ pub fn get_metadata_metaflac(path: String) -> AudioMetadata {
         duration: duration.unwrap_or_default(),
     };
     metadata
+}
+
+pub fn get_metadata_symphonia(path: String) -> AudioMetadata {
+    let src = std::fs::File::open(&path).expect("failed to open media");
+    let mss = MediaSourceStream::new(Box::new(src), Default::default());
+    let mut hint = Hint::new();
+    hint.with_extension("flac");
+    let meta_opts: MetadataOptions = Default::default();
+    let fmt_opts: FormatOptions = Default::default();
+    let mut probed = symphonia::default::get_probe()
+        .format(&hint, mss, &fmt_opts, &meta_opts)
+        .expect("unsupported format");
+
+    return if let Some(metadata_rev) = probed.format.metadata().current() {
+        get_tags(&metadata_rev)
+    /*         if probed.metadata.get().as_ref().is_some() {
+        println!("tags that are part of the container format are preferentially printed.");
+        println!("not printing additional tags that were found while probing.");
+    } */
+    } else if let Some(metadata_rev) = probed.metadata.get().as_ref().and_then(|m| m.current()) {
+        get_tags(&metadata_rev)
+    } else {
+        AudioMetadata::default()
+    };
+}
+fn get_tags(metadata_rev: &MetadataRevision) -> AudioMetadata {
+    let tags = metadata_rev.tags();
+    let mut metadata: AudioMetadata = AudioMetadata::default();
+    for tag in tags.iter() {
+        if let Some(std_key) = tag.std_key {
+            match std_key {
+                StandardTagKey::Album => metadata.album = tag.value.to_string(),
+                StandardTagKey::AcoustidFingerprint => todo!(),
+                StandardTagKey::AcoustidId => todo!(),
+                StandardTagKey::AlbumArtist => metadata.album_artist = tag.value.to_string(),
+                StandardTagKey::Arranger => todo!(),
+                StandardTagKey::Artist => (),
+                StandardTagKey::Bpm => todo!(),
+                StandardTagKey::Comment => (),
+                StandardTagKey::Compilation => todo!(),
+                StandardTagKey::Composer => (),
+                StandardTagKey::Conductor => todo!(),
+                StandardTagKey::ContentGroup => todo!(),
+                StandardTagKey::Copyright => todo!(),
+                StandardTagKey::Date => (),
+                StandardTagKey::Description => todo!(),
+                StandardTagKey::DiscNumber => (),
+                StandardTagKey::DiscSubtitle => todo!(),
+                StandardTagKey::DiscTotal => todo!(),
+                StandardTagKey::EncodedBy => todo!(),
+                StandardTagKey::Encoder => todo!(),
+                StandardTagKey::EncoderSettings => todo!(),
+                StandardTagKey::EncodingDate => todo!(),
+                StandardTagKey::Engineer => todo!(),
+                StandardTagKey::Ensemble => todo!(),
+                StandardTagKey::Genre => (),
+                StandardTagKey::IdentAsin => todo!(),
+                StandardTagKey::IdentBarcode => todo!(),
+                StandardTagKey::IdentCatalogNumber => todo!(),
+                StandardTagKey::IdentEanUpn => todo!(),
+                StandardTagKey::IdentIsrc => todo!(),
+                StandardTagKey::IdentPn => todo!(),
+                StandardTagKey::IdentPodcast => todo!(),
+                StandardTagKey::IdentUpc => todo!(),
+                StandardTagKey::Label => todo!(),
+                StandardTagKey::Language => todo!(),
+                StandardTagKey::License => todo!(),
+                StandardTagKey::Lyricist => todo!(),
+                StandardTagKey::Lyrics => todo!(),
+                StandardTagKey::MediaFormat => todo!(),
+                StandardTagKey::MixDj => todo!(),
+                StandardTagKey::MixEngineer => todo!(),
+                StandardTagKey::Mood => todo!(),
+                StandardTagKey::MovementName => todo!(),
+                StandardTagKey::MovementNumber => todo!(),
+                StandardTagKey::MusicBrainzAlbumArtistId => todo!(),
+                StandardTagKey::MusicBrainzAlbumId => todo!(),
+                StandardTagKey::MusicBrainzArtistId => todo!(),
+                StandardTagKey::MusicBrainzDiscId => todo!(),
+                StandardTagKey::MusicBrainzGenreId => todo!(),
+                StandardTagKey::MusicBrainzLabelId => todo!(),
+                StandardTagKey::MusicBrainzOriginalAlbumId => todo!(),
+                StandardTagKey::MusicBrainzOriginalArtistId => todo!(),
+                StandardTagKey::MusicBrainzRecordingId => todo!(),
+                StandardTagKey::MusicBrainzReleaseGroupId => todo!(),
+                StandardTagKey::MusicBrainzReleaseStatus => todo!(),
+                StandardTagKey::MusicBrainzReleaseTrackId => todo!(),
+                StandardTagKey::MusicBrainzReleaseType => todo!(),
+                StandardTagKey::MusicBrainzTrackId => todo!(),
+                StandardTagKey::MusicBrainzWorkId => todo!(),
+                StandardTagKey::Opus => todo!(),
+                StandardTagKey::OriginalAlbum => todo!(),
+                StandardTagKey::OriginalArtist => todo!(),
+                StandardTagKey::OriginalDate => todo!(),
+                StandardTagKey::OriginalFile => todo!(),
+                StandardTagKey::OriginalWriter => todo!(),
+                StandardTagKey::Owner => todo!(),
+                StandardTagKey::Part => todo!(),
+                StandardTagKey::PartTotal => todo!(),
+                StandardTagKey::Performer => todo!(),
+                StandardTagKey::Podcast => todo!(),
+                StandardTagKey::PodcastCategory => todo!(),
+                StandardTagKey::PodcastDescription => todo!(),
+                StandardTagKey::PodcastKeywords => todo!(),
+                StandardTagKey::Producer => todo!(),
+                StandardTagKey::PurchaseDate => todo!(),
+                StandardTagKey::Rating => todo!(),
+                StandardTagKey::ReleaseCountry => todo!(),
+                StandardTagKey::ReleaseDate => todo!(),
+                StandardTagKey::Remixer => todo!(),
+                StandardTagKey::ReplayGainAlbumGain => todo!(),
+                StandardTagKey::ReplayGainAlbumPeak => todo!(),
+                StandardTagKey::ReplayGainTrackGain => todo!(),
+                StandardTagKey::ReplayGainTrackPeak => todo!(),
+                StandardTagKey::Script => todo!(),
+                StandardTagKey::SortAlbum => todo!(),
+                StandardTagKey::SortAlbumArtist => todo!(),
+                StandardTagKey::SortArtist => todo!(),
+                StandardTagKey::SortComposer => todo!(),
+                StandardTagKey::SortTrackTitle => todo!(),
+                StandardTagKey::TaggingDate => todo!(),
+                StandardTagKey::TrackNumber => (),
+                StandardTagKey::TrackSubtitle => todo!(),
+                StandardTagKey::TrackTitle => metadata.name = tag.value.to_string(),
+                StandardTagKey::TrackTotal => todo!(),
+                StandardTagKey::TvEpisode => todo!(),
+                StandardTagKey::TvEpisodeTitle => todo!(),
+                StandardTagKey::TvNetwork => todo!(),
+                StandardTagKey::TvSeason => todo!(),
+                StandardTagKey::TvShowTitle => todo!(),
+                StandardTagKey::Url => todo!(),
+                StandardTagKey::UrlArtist => todo!(),
+                StandardTagKey::UrlCopyright => todo!(),
+                StandardTagKey::UrlInternetRadio => todo!(),
+                StandardTagKey::UrlLabel => todo!(),
+                StandardTagKey::UrlOfficial => todo!(),
+                StandardTagKey::UrlPayment => todo!(),
+                StandardTagKey::UrlPodcast => todo!(),
+                StandardTagKey::UrlPurchase => todo!(),
+                StandardTagKey::UrlSource => todo!(),
+                StandardTagKey::Version => todo!(),
+                StandardTagKey::Writer => todo!(),
+            }
+        }
+    }
+    return metadata;
 }
 
 fn get_year(vorbis: &VorbisComment) -> u32 {
@@ -118,12 +318,8 @@ pub fn get_metadata_lofty(path: String) -> AudioMetadata {
 
 pub fn get_metadata_lofty_specify_type(path: String) -> AudioMetadata {
     let file = std::fs::File::open(&path).unwrap();
-
-    let flac = FlacFile::read_from(
-        std::io::BufReader::new(file).get_mut(),
-        lofty::ParseOptions::new(),
-    )
-    .unwrap();
+    let mut reader = std::io::BufReader::new(file);
+    let flac = FlacFile::read_from(&mut reader, lofty::ParseOptions::new()).unwrap();
     let vorbis = flac.vorbis_comments().unwrap();
     let properties = flac.properties();
     let duration = properties.duration();
@@ -142,13 +338,26 @@ pub fn get_metadata_lofty_specify_type(path: String) -> AudioMetadata {
     metadata
 }
 
+pub fn get_metadata_taglib_rust(path: String) -> AudioMetadata {
+    let file = taglib::File::new(&path).unwrap();
+    let tags = file.tag().unwrap();
+    let metadata: AudioMetadata = AudioMetadata {
+        name: tags.title().unwrap().to_string(),
+        track: tags.track().unwrap(),
+        album: tags.album().unwrap().to_string(),
+        album_artist: tags.artist().unwrap(),
+        year: tags.year().unwrap(),
+        path,
+        lossless: true,
+        duration: file.audioproperties().unwrap().length().into(),
+    };
+
+    metadata
+}
+
 fn tag_reader(c: &mut Criterion) {
     let mut group = c.benchmark_group("Flac Tag Reader");
-    group.bench_function(stringify!("metaflac"), |b| {
-        b.iter(|| {
-            get_metadata_metaflac(Path::new("./full_test.flac").to_string_lossy().to_string())
-        })
-    });
+
     group.bench_function(stringify!("lofty"), |b| {
         b.iter(|| get_metadata_lofty(Path::new("./full_test.flac").to_string_lossy().to_string()))
     });
@@ -157,6 +366,28 @@ fn tag_reader(c: &mut Criterion) {
             get_metadata_lofty_specify_type(
                 Path::new("./full_test.flac").to_string_lossy().to_string(),
             )
+        })
+    });
+    group.bench_function(stringify!("symphonia"), |b| {
+        b.iter(|| {
+            get_metadata_symphonia(Path::new("./full_test.flac").to_string_lossy().to_string())
+        })
+    });
+    group.bench_function(stringify!("metaflac"), |b| {
+        b.iter(|| {
+            get_metadata_metaflac(Path::new("./full_test.flac").to_string_lossy().to_string())
+        })
+    });
+    group.bench_function(stringify!("metaflac read_from"), |b| {
+        b.iter(|| {
+            get_metadata_metaflac_reader(
+                Path::new("./full_test.flac").to_string_lossy().to_string(),
+            )
+        })
+    });
+    group.bench_function(stringify!("taglib-rust"), |b| {
+        b.iter(|| {
+            get_metadata_taglib_rust(Path::new("./full_test.flac").to_string_lossy().to_string())
         })
     });
     group.finish();
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,189 @@
 use lofty::{Accessor, AudioFile, Probe, TaggedFileExt};
 
-fn main() {
-    println!("Hello, world!");
+use symphonia::core::formats::FormatOptions;
+use symphonia::core::io::MediaSourceStream;
+use symphonia::core::meta::{MetadataOptions, MetadataRevision, StandardTagKey};
+use symphonia::core::probe::Hint;
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct AudioMetadata {
+    pub name: String,
+    pub artist: String,
+    pub album: String,
+    pub album_artist: String,
+    pub year: u32,
+    pub disc_number: String,
+    pub composer: String,
+    pub track: u32,
+    pub path: String,
+    pub lossless: bool,
+    pub duration: u64,
+    pub genres: String,
+    pub comment: String,
+}
+
+impl Default for AudioMetadata {
+    fn default() -> AudioMetadata {
+        AudioMetadata {
+            name: String::from(""),
+            artist: String::from(""),
+            composer: String::from(""),
+            disc_number: String::from(""),
+            album: String::from(""),
+            album_artist: String::from(""),
+            year: 0,
+            track: 0,
+            path: String::from(""),
+            lossless: false,
+            duration: 0,
+            genres: String::from(""),
+            comment: String::from(""),
+        }
+    }
+}
+pub fn main() {
+    let path: String = "./full_test.flac".to_string();
+    let src = std::fs::File::open(&path).expect("failed to open media");
+    let mss = MediaSourceStream::new(Box::new(src), Default::default());
+    let mut hint = Hint::new();
+    hint.with_extension("flac");
+    let meta_opts: MetadataOptions = Default::default();
+    let fmt_opts: FormatOptions = Default::default();
+    let mut probed = symphonia::default::get_probe()
+        .format(&hint, mss, &fmt_opts, &meta_opts)
+        .expect("unsupported format");
+
+    if let Some(metadata_rev) = probed.format.metadata().current() {
+        let metadata = get_tags(&metadata_rev);
+        println!("{:?}", metadata);
+        if probed.metadata.get().as_ref().is_some() {
+            println!("tags that are part of the container format are preferentially printed.");
+            println!("not printing additional tags that were found while probing.");
+        }
+    } else if let Some(metadata_rev) = probed.metadata.get().as_ref().and_then(|m| m.current()) {
+        let metadata = get_tags(&metadata_rev);
+        println!("{:?}", metadata);
+    }
+}
+
+fn get_tags(metadata_rev: &MetadataRevision) -> AudioMetadata {
+    let tags = metadata_rev.tags();
+    let mut metadata: AudioMetadata = AudioMetadata::default();
+    for tag in tags.iter() {
+        if let Some(std_key) = tag.std_key {
+            match std_key {
+                StandardTagKey::Album => metadata.album = tag.value.to_string(),
+                StandardTagKey::AcoustidFingerprint => todo!(),
+                StandardTagKey::AcoustidId => todo!(),
+                StandardTagKey::AlbumArtist => metadata.album_artist = tag.value.to_string(),
+                StandardTagKey::Arranger => todo!(),
+                StandardTagKey::Artist => metadata.artist = tag.value.to_string(),
+                StandardTagKey::Bpm => todo!(),
+                StandardTagKey::Comment => metadata.comment = tag.value.to_string(),
+                StandardTagKey::Compilation => todo!(),
+                StandardTagKey::Composer => metadata.composer = tag.value.to_string(),
+                StandardTagKey::Conductor => todo!(),
+                StandardTagKey::ContentGroup => todo!(),
+                StandardTagKey::Copyright => todo!(),
+                StandardTagKey::Date => (),
+                StandardTagKey::Description => todo!(),
+                StandardTagKey::DiscNumber => metadata.disc_number = tag.value.to_string(),
+                StandardTagKey::DiscSubtitle => todo!(),
+                StandardTagKey::DiscTotal => todo!(),
+                StandardTagKey::EncodedBy => todo!(),
+                StandardTagKey::Encoder => todo!(),
+                StandardTagKey::EncoderSettings => todo!(),
+                StandardTagKey::EncodingDate => todo!(),
+                StandardTagKey::Engineer => todo!(),
+                StandardTagKey::Ensemble => todo!(),
+                StandardTagKey::Genre => metadata.genres = tag.value.to_string(),
+                StandardTagKey::IdentAsin => todo!(),
+                StandardTagKey::IdentBarcode => todo!(),
+                StandardTagKey::IdentCatalogNumber => todo!(),
+                StandardTagKey::IdentEanUpn => todo!(),
+                StandardTagKey::IdentIsrc => todo!(),
+                StandardTagKey::IdentPn => todo!(),
+                StandardTagKey::IdentPodcast => todo!(),
+                StandardTagKey::IdentUpc => todo!(),
+                StandardTagKey::Label => todo!(),
+                StandardTagKey::Language => todo!(),
+                StandardTagKey::License => todo!(),
+                StandardTagKey::Lyricist => todo!(),
+                StandardTagKey::Lyrics => todo!(),
+                StandardTagKey::MediaFormat => todo!(),
+                StandardTagKey::MixDj => todo!(),
+                StandardTagKey::MixEngineer => todo!(),
+                StandardTagKey::Mood => todo!(),
+                StandardTagKey::MovementName => todo!(),
+                StandardTagKey::MovementNumber => todo!(),
+                StandardTagKey::MusicBrainzAlbumArtistId => todo!(),
+                StandardTagKey::MusicBrainzAlbumId => todo!(),
+                StandardTagKey::MusicBrainzArtistId => todo!(),
+                StandardTagKey::MusicBrainzDiscId => todo!(),
+                StandardTagKey::MusicBrainzGenreId => todo!(),
+                StandardTagKey::MusicBrainzLabelId => todo!(),
+                StandardTagKey::MusicBrainzOriginalAlbumId => todo!(),
+                StandardTagKey::MusicBrainzOriginalArtistId => todo!(),
+                StandardTagKey::MusicBrainzRecordingId => todo!(),
+                StandardTagKey::MusicBrainzReleaseGroupId => todo!(),
+                StandardTagKey::MusicBrainzReleaseStatus => todo!(),
+                StandardTagKey::MusicBrainzReleaseTrackId => todo!(),
+                StandardTagKey::MusicBrainzReleaseType => todo!(),
+                StandardTagKey::MusicBrainzTrackId => todo!(),
+                StandardTagKey::MusicBrainzWorkId => todo!(),
+                StandardTagKey::Opus => todo!(),
+                StandardTagKey::OriginalAlbum => todo!(),
+                StandardTagKey::OriginalArtist => todo!(),
+                StandardTagKey::OriginalDate => todo!(),
+                StandardTagKey::OriginalFile => todo!(),
+                StandardTagKey::OriginalWriter => todo!(),
+                StandardTagKey::Owner => todo!(),
+                StandardTagKey::Part => todo!(),
+                StandardTagKey::PartTotal => todo!(),
+                StandardTagKey::Performer => todo!(),
+                StandardTagKey::Podcast => todo!(),
+                StandardTagKey::PodcastCategory => todo!(),
+                StandardTagKey::PodcastDescription => todo!(),
+                StandardTagKey::PodcastKeywords => todo!(),
+                StandardTagKey::Producer => todo!(),
+                StandardTagKey::PurchaseDate => todo!(),
+                StandardTagKey::Rating => todo!(),
+                StandardTagKey::ReleaseCountry => todo!(),
+                StandardTagKey::ReleaseDate => todo!(),
+                StandardTagKey::Remixer => todo!(),
+                StandardTagKey::ReplayGainAlbumGain => todo!(),
+                StandardTagKey::ReplayGainAlbumPeak => todo!(),
+                StandardTagKey::ReplayGainTrackGain => todo!(),
+                StandardTagKey::ReplayGainTrackPeak => todo!(),
+                StandardTagKey::Script => todo!(),
+                StandardTagKey::SortAlbum => todo!(),
+                StandardTagKey::SortAlbumArtist => todo!(),
+                StandardTagKey::SortArtist => todo!(),
+                StandardTagKey::SortComposer => todo!(),
+                StandardTagKey::SortTrackTitle => todo!(),
+                StandardTagKey::TaggingDate => todo!(),
+                StandardTagKey::TrackNumber => (),
+                StandardTagKey::TrackSubtitle => todo!(),
+                StandardTagKey::TrackTitle => metadata.name = tag.value.to_string(),
+                StandardTagKey::TrackTotal => todo!(),
+                StandardTagKey::TvEpisode => todo!(),
+                StandardTagKey::TvEpisodeTitle => todo!(),
+                StandardTagKey::TvNetwork => todo!(),
+                StandardTagKey::TvSeason => todo!(),
+                StandardTagKey::TvShowTitle => todo!(),
+                StandardTagKey::Url => todo!(),
+                StandardTagKey::UrlArtist => todo!(),
+                StandardTagKey::UrlCopyright => todo!(),
+                StandardTagKey::UrlInternetRadio => todo!(),
+                StandardTagKey::UrlLabel => todo!(),
+                StandardTagKey::UrlOfficial => todo!(),
+                StandardTagKey::UrlPayment => todo!(),
+                StandardTagKey::UrlPodcast => todo!(),
+                StandardTagKey::UrlPurchase => todo!(),
+                StandardTagKey::UrlSource => todo!(),
+                StandardTagKey::Version => todo!(),
+                StandardTagKey::Writer => todo!(),
+            }
+        }
+    }
+    return metadata;
 }
